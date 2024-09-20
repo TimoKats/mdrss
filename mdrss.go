95c807f31c92bc1748594c62bbd131953454ed07
Timo Kats <31708538+TimoKats@users.noreply.github.com>
2024-09-20 21:56:49 +0200

    add init command, Co-authored-by: varphi-online <nbash1984@gmail.com> (#32)
    * add init command
    
    * Some more fixes, trying to give credit.
    
    ---------
    
    Co-authored-by: varphi-online <nbash1984@gmail.com>

--- mdrss.go
+++ mdrss.go
@@ -2,56 +2,20 @@ package main
 
 import (
   mdrss "github.com/TimoKats/mdrss/lib"
-  "reflect"
   "errors"
   "os"
 )
 
-func lsCommand(config mdrss.Config) error {
-  files, fileErr := mdrss.GetArticles(config)
-  if fileErr == nil {
-    for _, file := range files {
-      mdrss.Info.Println(file)
-    }
-  }
-  return fileErr
-}
-
-func updateCommand(config mdrss.Config) error {
-  files, fileErr := mdrss.GetArticles(config)
-  if fileErr == nil {
-    config.Articles = mdrss.ReadMarkdown(config, files)
-    rssXml := mdrss.CreateRSS(config)
-    rssErr := mdrss.WriteRSS(rssXml, config)
-    if rssErr != nil {
-      return rssErr
-    }
-    mdrss.Info.Printf("Content written to %s", config.OutputFile)
-  }
-  return fileErr
-}
-
-func confCommand(config mdrss.Config) error {
-	configValues := reflect.ValueOf(config)
-  typeOfS := configValues.Type()
-  for i := 0; i < configValues.NumField(); i++ {
-    if len(typeOfS.Field(i).Name) < 8 {
-      mdrss.Info.Printf("%s\t\t%v\n", typeOfS.Field(i).Name, configValues.Field(i).Interface())
-    } else {
-      mdrss.Info.Printf("%s\t%v\n", typeOfS.Field(i).Name, configValues.Field(i).Interface())
-    }
-  }
-  return nil
-}
-
 func parseCommand(command string, config mdrss.Config) error {
   switch (command) {
     case "ls":
-      return lsCommand(config)
+      return mdrss.LsCommand(config)
     case "conf":
-      return confCommand(config)
+      return mdrss.ConfCommand(config)
     case "update":
-      return updateCommand(config)
+      return mdrss.UpdateCommand(config)
+    case "init":
+      return mdrss.InitCommand()
     default:
       return errors.New("Command not found.")
   }
@@ -61,7 +25,7 @@ func main() {
   arguments, argumentErr := mdrss.ParseArguments(os.Args)
   config, configErr := mdrss.ReadConfig(*arguments["config"])
   mdrss.Info.Printf("Using config location: %v", *arguments["config"])
-  if err := errors.Join(argumentErr, configErr); err != nil {
+  if err := errors.Join(argumentErr, configErr); err != nil && *arguments["command"] != "init" {
      mdrss.Error.Println(err)
      return
    }
